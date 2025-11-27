9299fe1d7fd21a8c449b71a71b001d05f5d6eb76
Mihail-Gyrla <mihailgyrla@gmail.com>
2025-11-27 21:19:07 +0300

    commit

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -1,9 +1,12 @@
+import pytest
 import requests
-
+@pytest.mark.xfail(reason = 200)
 def test_add():
-    body = {"title":"generated","completed":False}
+    body = {"title":"Михаил","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
-    
-    assert response.status_code == 202
-    assert response_body['completed'] == False
+    id = response.json()["id"]
+    response = requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
+    response = requests.get(f"https://todo-app-sky.herokuapp.com/{id}")
+    assert response.status_code == 404
+
