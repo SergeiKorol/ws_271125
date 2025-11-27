86f73531eb2876065600ec59602f7092b2240ed6
Mihail-Gyrla <mihailgyrla@gmail.com>
2025-11-27 21:30:24 +0300

    commit

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 @pytest.mark.xfail(reason = 200)
+'''Код с багом'''
 def test_add():
     body = {"title":"Михаил","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
